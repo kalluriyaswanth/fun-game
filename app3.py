cde0d5fdd559531849728584077bc2b8acffcb86
Kalluri Yaswanth <106922361+kalluriyaswanth@users.noreply.github.com>
2023-03-01 18:19:11 +0530

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.title(':red[Amma] I owe to you forever')
+st.header('Data Science Intern')
+st.subheader('Feb 2023:sunglasses:')
+st.snow()
+code='''print('hi amma')'''
+st.code(code, language='cpp')
+import streamlit as st
+
+if st.button('Say hello'):
+    st.write('Why hello there')
+    st.balloons()
+else:
+    st.write('Goodbye')
+
